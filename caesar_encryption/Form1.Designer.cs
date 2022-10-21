dfa2cf4884e580cc0d9de2700613c6b5fa1c43ca
Artem Bychkov <48648751+Frostray1@users.noreply.github.com>
2022-10-21 15:28:24 +0300

    Add files via upload

---
+++ caesar_encryption/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Лаба_по_шифрованию_цезаря_с_ключом
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtForKey = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCode = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtForSourceText = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.zakodtext = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtForKey
+            // 
+            this.txtForKey.Location = new System.Drawing.Point(12, 25);
+            this.txtForKey.Name = "txtForKey";
+            this.txtForKey.Size = new System.Drawing.Size(100, 20);
+            this.txtForKey.TabIndex = 0;
+            this.txtForKey.TextChanged += new System.EventHandler(this.txtForKey_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ключ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ключевое слово";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCode
+            // 
+            this.btnCode.Location = new System.Drawing.Point(346, 23);
+            this.btnCode.Name = "btnCode";
+            this.btnCode.Size = new System.Drawing.Size(75, 23);
+            this.btnCode.TabIndex = 6;
+            this.btnCode.Text = "Шифровать";
+            this.btnCode.UseVisualStyleBackColor = true;
+            this.btnCode.Click += new System.EventHandler(this.btnCode_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(427, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Расшифровать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Исходный текст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(635, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Закодированный алфавит";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(312, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Закодированный текст";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(644, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Раскодированный текст";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtForSourceText
+            // 
+            this.txtForSourceText.Location = new System.Drawing.Point(12, 87);
+            this.txtForSourceText.Multiline = true;
+            this.txtForSourceText.Name = "txtForSourceText";
+            this.txtForSourceText.Size = new System.Drawing.Size(266, 426);
+            this.txtForSourceText.TabIndex = 21;
+            this.txtForSourceText.TextChanged += new System.EventHandler(this.txtForSourceText_TextChanged);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(647, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 20);
+            this.textBox1.TabIndex = 25;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // zakodtext
+            // 
+            this.zakodtext.Location = new System.Drawing.Point(315, 87);
+            this.zakodtext.Multiline = true;
+            this.zakodtext.Name = "zakodtext";
+            this.zakodtext.Size = new System.Drawing.Size(266, 426);
+            this.zakodtext.TabIndex = 26;
+            this.zakodtext.TextChanged += new System.EventHandler(this.zakodtext_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(647, 87);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(266, 426);
+            this.textBox3.TabIndex = 27;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 626);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.zakodtext);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtForSourceText);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnCode);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtForKey);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCode;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtForSourceText;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        public System.Windows.Forms.TextBox txtForKey;
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox zakodtext;
+        public System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ caesar_encryption/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаба_по_шифрованию_цезаря_с_ключом
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Caesar caesar = new Caesar();
+        }
+        string fileText;
+        string filename;
+
+        private static string alpha = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя ";
+        private static char[] newAlpha = new char[34];
+
+
+        public static string encrypt(string Message)
+        {
+            string res = "";
+            foreach (char ch in Message)
+            {
+                for (int i = 0; i < alpha.Length; i++)
+                {
+                    if (ch == alpha[i])
+                    {
+                        res += newAlpha[i];
+                        break;
+                    }
+                    // if (ch == 32) res += ' ';
+                }
+            }
+            return res;
+        }
+
+        public static string decrypt(string Message)
+        {
+            string res = "";
+            foreach (char ch in Message)
+            {
+                for (int i = 0; i < newAlpha.Length; i++)
+                {
+                    if (ch == newAlpha[i])
+                    {
+                        res += alpha[i];
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+
+        public static void createNewAlpha(string keyWord, int key) // создаёт новый алфавит с помощью ключа
+        {  
+            
+            bool findSame = false;
+            key--;
+            int beg = 0, current = key;
+            // добавить ключевое слово в новый алфавит
+            for (int i = key; i < keyWord.Length + key; i++)
+            {
+                for (int j = key; j < keyWord.Length + key; j++)
+                {
+                    if (keyWord[i - key] == newAlpha[j])
+                    {
+                        findSame = true;
+                        break;
+                    }
+                }
+                if (!findSame)// если повторений нету, то буква добавляется в новый алфавит
+                {
+                    newAlpha[current] = keyWord[i - key];
+                    current++;
+                }
+                findSame = false;
+            }
+
+            // добавить буквы после ключевого слова
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                for (int j = 0; j < newAlpha.Length; j++)
+                {
+                    if (alpha[i] == newAlpha[j])
+                    {
+                        findSame = true;
+                        break;
+                    }
+                }
+                if (!findSame)
+                {
+                    newAlpha[current] = alpha[i];
+                    current++;
+                }
+                findSame = false;
+                if (current == newAlpha.Length)
+                {
+                    beg = i;
+                    break;
+                }
+            }
+
+            // добавить буквы после ключевого слова
+            current = 0;
+            for (int i = beg; i < alpha.Length; i++)
+            {
+                for (int j = 0; j < newAlpha.Length; j++)
+                {
+                    if (alpha[i] == newAlpha[j])
+                    {
+                        findSame = true;
+                        break;
+                    }
+                }
+                if (!findSame)
+                {
+                    newAlpha[current] = alpha[i];
+                    current++;
+                }
+                findSame = false;
+            }
+        }
+
+        public static string getNewAlpha()
+        {
+            string strNewAlpha = new string(newAlpha);
+            return strNewAlpha;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            if (openFile.ShowDialog() == DialogResult.Cancel) return;
+            else
+            {
+                filename = openFile.FileName;
+                // читаем файл в строку
+                //fileText = System.IO.File.ReadAllText(filename);
+                fileText = System.IO.File.ReadAllText(filename, Encoding.GetEncoding(1251));
+
+                txtForSourceText.Text = fileText;
+
+
+                MessageBox.Show("Файл открыт", fileText);
+            }
+        }
+
+       
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCode_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = " ";
+            createNewAlpha(textBox2.Text, Convert.ToInt32(txtForKey.Text));
+            textBox1.Text = getNewAlpha();
+            zakodtext.Text = encrypt(fileText);
+             MessageBox.Show(txtForSourceText.Text);
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+
+            if (saveFile.ShowDialog() == DialogResult.Cancel) return;
+            else
+            {
+                string filename = saveFile.FileName;
+                System.IO.File.WriteAllText(filename, zakodtext.Text, Encoding.GetEncoding(1251)); // потом доделать с сохранением шифрованного файла
+                MessageBox.Show("Зашифрованный файл сохранен");
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtForKey_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtForSourceText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zakodtext_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            zakodtext.Text = " ";
+            createNewAlpha(textBox2.Text, Convert.ToInt32(txtForKey.Text));
+            textBox1.Text = getNewAlpha();
+            textBox3.Text= decrypt(fileText);
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+
+            if (saveFile.ShowDialog() == DialogResult.Cancel) return;
+            else
+            {
+                string filename = saveFile.FileName;
+                System.IO.File.WriteAllText(filename, textBox3.Text, Encoding.GetEncoding(1251)); // потом доделать с сохранением шифрованного файла
+                MessageBox.Show("Зашифрованный файл сохранен");
+            }
+        }
+
+        private void textBox3_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Caesar
+    {
+        private static string alpha = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        private static char[] newAlpha = new char[32];
+
+        public static string encrypt(string Message)
+        {
+            string res = "";
+            foreach (char ch in Message)
+            {
+                for (int i = 0; i < alpha.Length; i++)
+                {
+                    if (ch == alpha[i])
+                    {
+                        res += newAlpha[i];
+                        break;
+                    }
+                    // if (ch == 32) res += ' ';
+                }
+            }
+            return res;
+        }
+
+        public static string decrypt(string Message)
+        {
+            string res = "";
+            foreach (char ch in Message)
+            {
+                for (int i = 0; i < newAlpha.Length; i++)
+                {
+                    if (ch == newAlpha[i])
+                    {
+                        res += alpha[i];
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+
+        public static void createNewAlpha(string keyWord, int key) // создаёт новый алфавит с помощью ключа
+        {
+            bool findSame = false;
+            key--;
+            int beg = 0, current = key;
+            // добавить ключевое слово в новый алфавит
+            for (int i = key; i < keyWord.Length + key; i++)
+            {
+                for (int j = key; j < keyWord.Length + key; j++)
+                {
+                    if (keyWord[i - key] == newAlpha[j])
+                    {
+                        findSame = true;
+                        break;
+                    }
+                }
+                if (!findSame)// если повторений нету, то буква добавляется в новый алфавит
+                {
+                    newAlpha[current] = keyWord[i - key];
+                    current++;
+                }
+                findSame = false;
+            }
+
+            // добавить буквы после ключевого слова
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                for (int j = 0; j < newAlpha.Length; j++)
+                {
+                    if (alpha[i] == newAlpha[j])
+                    {
+                        findSame = true;
+                        break;
+                    }
+                }
+                if (!findSame)
+                {
+                    newAlpha[current] = alpha[i];
+                    current++;
+                }
+                findSame = false;
+                if (current == newAlpha.Length)
+                {
+                    beg = i;
+                    break;
+                }
+            }
+
+            // добавить буквы после ключевого слова
+            current = 0;
+            for (int i = beg; i < alpha.Length; i++)
+            {
+                for (int j = 0; j < newAlpha.Length; j++)
+                {
+                    if (alpha[i] == newAlpha[j])
+                    {
+                        findSame = true;
+                        break;
+                    }
+                }
+                if (!findSame)
+                {
+                    newAlpha[current] = alpha[i];
+                    current++;
+                }
+                findSame = false;
+            }
+        }
+
+        public static string getNewAlpha()
+        {
+            string strNewAlpha = new string(newAlpha);
+            return strNewAlpha;
+        }
+    }
+}
